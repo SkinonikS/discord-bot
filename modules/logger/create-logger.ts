ba1345baf2298128ebd95d9af8ef7f5b93acd829
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-05-31 01:26:48 +0300

    refactor: remove new line

--- modules/logger/create-logger.ts
+++ modules/logger/create-logger.ts
@@ -3,7 +3,6 @@ import { LoggerFactoryInterface, LoggerInterface } from '#modules/logger/types';
 
 export const createLogger = (source: string, app?: Application): LoggerInterface => {
   app ??= Application.getInstance();
-
   const loggerFactory = app.container.get<LoggerFactoryInterface>('Logger.Factory');
   return loggerFactory.createLogSource(source);
 };
