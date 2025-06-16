c411a896a513ac7f55d761076f0d18606e179d0c
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-06-16 22:24:35 +0300

    fix: add reflect-metadata

--- apps/bot/bin/start-bot.ts
+++ apps/bot/bin/start-bot.ts
@@ -2,6 +2,7 @@ import path from 'node:path';
 import { Application } from '@framework/core';
 import pkg from '../package.json';
 import { createKernel } from '#start/kernel';
+import 'reflect-metadata';
 
 const app = new Application(path.resolve(import.meta.dirname, '..'), pkg.version);
 Application.setInstance(app);
