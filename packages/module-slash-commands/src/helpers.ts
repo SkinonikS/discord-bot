6d58f521427ecbcaae0a3e072cfc2b92ed026bc2
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-06-21 13:31:36 +0300

    feat: add `getSlashCommandManager` helper function

--- packages/module-slash-commands/src/helpers.ts
+++ packages/module-slash-commands/src/helpers.ts
@@ -1,5 +1,11 @@
-import { defineBaseConfig } from '@framework/core';
+import { Application, defineBaseConfig } from '@framework/core';
+import type SlashCommandManager from '#/slash-command-manager';
 import type { SlashCommandConfig } from '#/types';
+
+export const getSlashCommandManager = (app?: Application): Promise<SlashCommandManager> => {
+  app = app ?? Application.getInstance();
+  return app.container.make('slash-commands');
+};
 
 export const defineSlashCommandsConfig = (config: Partial<SlashCommandConfig>) => defineBaseConfig<SlashCommandConfig>('slash-commands', {
   commands: config.commands ?? [],
