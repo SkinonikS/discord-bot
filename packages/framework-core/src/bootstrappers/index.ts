0cde0ede452fbaf20843efb0eab7f292b45079b7
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-06-21 11:57:45 +0300

    feat: improve export

---
+++ packages/framework-core/src/bootstrappers/index.ts
@@ -0,0 +1,5 @@
+export { default as BootModules } from '#/bootstrappers/boot-modules';
+export { default as HandleErrors } from '#/bootstrappers/handle-errors';
+export { default as LoadConfiguration } from '#/bootstrappers/load-configuration';
+export { default as LoadEnvironmentVariables } from '#/bootstrappers/load-environment-variables';
+export { default as RegisterModules } from '#/bootstrappers/register-modules';
