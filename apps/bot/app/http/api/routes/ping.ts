13967b74323176a4cae5bb048f5fcbc4faacae5a
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-06-23 13:28:42 +0300

    fix(app/bot): fix binding name

--- apps/bot/app/http/api/routes/ping.ts
+++ apps/bot/app/http/api/routes/ping.ts
@@ -1,7 +1,7 @@
 import { defineEventHandler } from 'h3';
 
 export default defineEventHandler(async (event) => {
-  const discord = await event.context.container.make('discord');
+  const discord = await event.context.container.make('discord.client');
   return {
     isReady: discord.isReady(),
     uptime: discord.uptime ?? 0,
