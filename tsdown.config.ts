f1fc0fe8d2675be4135b9dcac3df0babc31fd8d7
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-06-26 18:03:02 +0300

    chore: make build silent

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsdown/config';
 
 export default defineConfig({
+  silent: true,
   workspace: true,
   external: (id: string) => {
     return id.startsWith('@app/') || id.startsWith('@module/') || id.startsWith('@framework/');
