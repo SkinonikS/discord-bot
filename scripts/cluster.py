c100ff705515444166a36d80cbd90e22c8b066b6
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-06-28 17:06:32 +0300

    refactor(scripts): improve build scripts

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -5,6 +5,7 @@ import typer
 app = typer.Typer(add_completion=False)
 
 @app.command()
+@app.command("i")
 def install(
   release_name: str = "production",
   namespace: str = "discord-bot-cluster",
@@ -13,10 +14,10 @@ def install(
   """
   Install or upgrade the Discord bot cluster Helm release.
   """
-  print(f"Installing or upgrading the Discord bot cluster Helm release: {release_name} in namespace: {namespace}")
-  subprocess.run(['helm', 'upgrade', '--install', release_name, charts_dir, '--namespace', namespace, '--create-namespace'])
+  subprocess.run(['helm', 'upgrade', '--install', release_name, charts_dir, '--namespace', namespace, '--create-namespace'], check=True)
 
 @app.command()
+@app.command("u")
 def uninstall(
   release_name: str = "production",
   namespace: str = "discord-bot-cluster"
@@ -24,12 +25,12 @@ def uninstall(
   """
   Uninstall the Discord bot cluster Helm release.
   """
-  print("Warning: This will remove the Discord bot cluster and all associated resources.")
-  subprocess.run(['helm', 'uninstall', release_name, '--namespace', namespace])
+  subprocess.run(['helm', 'uninstall', release_name, '--namespace', namespace], check=True)
 
 @app.command()
+@app.command("b")
 def build(
-  no_cache: bool = True,
+  cache: bool = False,
   image_name: str = "discord-bot:latest",
   target: str = "discord-bot",
   auto_install: bool = False
@@ -37,12 +38,12 @@ def build(
   """
   Build the Docker image for the Discord bot operator.
   """
-  cache_option = "--no-cache" if no_cache else ""
-  print(f"Building the Docker image: {image_name} with target: {target} {'without cache' if no_cache else 'with cache'}")
-  subprocess.run(["docker", "build", "../", "--target", target, "--tag", image_name, cache_option])
+  command = ["docker", "build", "../", "--target", target, "--tag", image_name]
+  if cache is False:
+    command.append("--no-cache")
+  subprocess.run(command, check=True)
 
   if auto_install:
-    print(f"Installing the Docker image: {image_name} to the cluster")
     install()
 
 if __name__ == "__main__":
