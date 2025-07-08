a6aec3b4e1fe0cb64a27e9f923137873fea744d9
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-07-08 19:37:52 +0300

    use pino logger in logger configuration

--- apps/discord-bot/config/logger.ts
+++ apps/discord-bot/config/logger.ts
@@ -1,14 +1,18 @@
-import { defineLoggerConfig } from '@module/logger/config';
-import type { LoggerConfig } from '@module/logger/config';
-import ConsoleTransportFactory from '#app/logger/transports/console-transport-factory';
-import { Env } from '#bootstrap/env';
+import { defineLoggerConfig, PinoLoggerDriver, type LogLevel } from '@module/logger/config';
+import { ConsoleTransportFactory } from '@module/logger/pino';
+import { Env } from '#root/bootstrap/env';
 import pkg from '#root/package.json';
 
 export default defineLoggerConfig({
-  label: pkg.name,
-  level: Env.LOG_LEVEL as LoggerConfig['level'],
-  showStackTraces: Env.LOG_SHOW_STACK_TRACES,
-  transports: [
-    new ConsoleTransportFactory(),
-  ],
+  defaultTags: {
+    app: pkg.name,
+    version: pkg.version,
+  },
+  driver: new PinoLoggerDriver({
+    level: Env.LOG_LEVEL as LogLevel,
+    stackTraces: Env.LOG_SHOW_STACK_TRACES,
+    transports: [
+      new ConsoleTransportFactory(),
+    ],
+  }),
 });
