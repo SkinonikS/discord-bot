f3699aa1bb39757e86ee4643e69e18b399373146
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-07-09 00:18:24 +0300

    add custom rate limit message

--- apps/discord-bot/config/slash-commands.ts
+++ apps/discord-bot/config/slash-commands.ts
@@ -1,13 +1,18 @@
+import type { Application } from '@framework/core/app';
 import { defineSlashCommandsConfig, RedisRateLimiterDriver } from '@module/slash-commands/config';
 
 export default defineSlashCommandsConfig({
   commands: [
     () => import('#app/slash-commands/ping'),
-    () => import('#app/slash-commands/music'),
+    () => import('#app/slash-commands/purge'),
   ],
   rateLimiter: {
     driver: new RedisRateLimiterDriver({ client: 'default' }),
     points: 1,
     durationMs: 5000,
+    message: async (app: Application, expiredTimestamp: number, locale: string) => {
+      const i18n = await app.container.make('i18n');
+      return i18n.t('slashCommands.rateLimit', { lng: locale, timestamp: expiredTimestamp });
+    },
   },
 });
