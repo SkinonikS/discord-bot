b754a36ffef807c97ba9a8982ea497dba7cd33ca
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-07-14 23:45:57 +0300

    add return type

--- apps/discord-bot/config/slash-commands.ts
+++ apps/discord-bot/config/slash-commands.ts
@@ -10,7 +10,7 @@ export default defineSlashCommandsConfig({
     driver: new RedisRateLimiterDriver({ client: 'default' }),
     points: 1,
     durationMs: 5000,
-    message: async (app: Application, expiredTimestamp: number, locale: string) => {
+    message: async (app: Application, expiredTimestamp: number, locale: string): Promise<string> => {
       const i18n = await app.container.make('i18n');
       return i18n.t('slashCommands.rateLimit', { lng: locale, timestamp: expiredTimestamp });
     },
