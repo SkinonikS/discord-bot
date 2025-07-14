0a828219f967ef87cafb61ab9ddc7c7028208319
Vadim SkinonikS.# <skinnoniks@gmail.com>
2025-07-15 00:22:11 +0300

    add important comments to http api routes

--- apps/discord-bot/app/http/router.ts
+++ apps/discord-bot/app/http/router.ts
@@ -2,14 +2,20 @@ import { createRouter, defineLazyEventHandler, useBase } from '@module/http-api/
 
 const router = createRouter();
 
+// used in Kubernetes readiness probes
+// DO NOT REMOVE UNLESS YOU KNOW WHAT YOU ARE DOING
 router.get('/livez', defineLazyEventHandler(() => {
   return import('#app/http/routes/livez').then(module => module.default);
 }));
 
+// used in Kubernetes readiness probes
+// DO NOT REMOVE UNLESS YOU KNOW WHAT YOU ARE DOING
 router.get('/readyz', defineLazyEventHandler(() => {
   return import('#app/http/routes/readyz').then(module => module.default);
 }));
 
+// used for prometheus metrics
+// DO NOT REMOVE UNLESS YOU KNOW WHAT YOU ARE DOING
 router.get('/metrics', defineLazyEventHandler(() => {
   return import('#app/http/routes/metrics').then(module => module.default);
 }));
